69f9090c6d955cdcdcb162a0f42e6af3d4ecfee3
DaYuan <sduum82@gmail.com>
2021-06-02 16:29:22 +0800

    fix code style

--- example/hello_chrysalis.js
+++ example/hello_chrysalis.js
@@ -6,8 +6,8 @@
 const Transaction = require('../src/index');
 
 let index = "Lifebox"
-let rawMsg = { "hash":"TTEVBMMPUEOTSEIXPVCTFEVEWCWBMXTULBACGFRQK9TRGJEIIMCLBKW9CXWMNPDX" };
+let rawMsg = { "hash": "TTEVBMMPUEOTSEIXPVCTFEVEWCWBMXTULBACGFRQK9TRGJEIIMCLBKW9CXWMNPDX" };
 let signature = "4382440611608ed43b44275c7e7eb285b926e8c17a20s00beecbb86b4a2d13da3";
 
-Transaction.sendMessage(index,rawMsg,signature) 
+Transaction.sendMessage(index, rawMsg, signature).then((result) => console.log(result));
 
